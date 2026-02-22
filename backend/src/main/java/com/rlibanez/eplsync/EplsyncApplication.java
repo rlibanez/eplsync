743392b0369b31ff89f38a261491ee592f32d13b
rlibanez <cantavro@gmail.com>
2026-02-22 19:41:48 +0100

    chore: initial project structure for Spring Boot backend

---
+++ backend/src/main/java/com/rlibanez/eplsync/EplsyncApplication.java
@@ -0,0 +1,13 @@
+package com.rlibanez.eplsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplsyncApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rlibanez/eplsync/EplsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlibanez.eplsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
